166ed676dcebb3e281819e43dbadb8f9c6fa1f19
Marc Aupont <marc@digimarktech.com>
2017-03-17 12:08:31 -0400

    data model created and generating tasks from data source

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Skeleton Project</h1>'
+    templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+
+    private tasks = [
+
+        new Task(
+            "Buy a moneky",
+            false
+        ),
+        new Task(
+            "Walk the turtle",
+            false
+        )
+    ]
+}
